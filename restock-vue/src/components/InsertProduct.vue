e039028dcfe73b9e70148ce1b688f64f7bc7f773
leesk9719 <leesk9719@gmail.com>
2020-03-26 02:56:21 +0900

    url validation

--- restock-vue/src/components/InsertProduct.vue
+++ restock-vue/src/components/InsertProduct.vue
@@ -98,8 +98,18 @@ export default {
       }
     },
     isValidUrl(string) {
-      var pattern = new RegExp("^(http|https)://", "i");
-      return !!pattern.test(string);
+      let httpPattern = new RegExp("^(http|https)://", "i");
+      let urlPattern = new RegExp(
+        "^(https?:\\/\\/)?" + // protocol
+        "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // domain name
+        "((\\d{1,3}\\.){3}\\d{1,3}))" + // OR ip (v4) address
+        "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // port and path
+        "(\\?[;&a-z\\d%_.~+=-]*)?" + // query string
+          "(\\#[-a-z\\d_]*)?$",
+        "i"
+      );
+
+      return !!httpPattern.test(string) && !!urlPattern.test(string);
     }
   },
   mounted() {
