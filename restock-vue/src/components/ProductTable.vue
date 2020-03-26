f92341bb7181efe21bcda67cedc004560bed685c
leesk9719 <leesk9719@gmail.com>
2020-03-27 01:58:54 +0900

    css 수정

--- restock-vue/src/components/ProductTable.vue
+++ restock-vue/src/components/ProductTable.vue
@@ -1,54 +1,60 @@
 <template>
-  <v-simple-table id="product-table">
-    <template v-slot:default>
-      <thead>
-        <tr>
-          <th class="text-left">#</th>
-          <th class="text-left">상품 이미지</th>
-          <th class="text-left">상품명</th>
-          <th class="text-left">삭제</th>
-        </tr>
-      </thead>
-      <tbody id="product-rows">
-        <tr v-for="(product, index) in products" :key="index">
-          <th scope="row">{{ product.prdId }}</th>
-          <td>
-            <img :src="product.prdImg" width="100px" />
-          </td>
-          <td>
-            <a :href="product.prdUrl" target="_blank">{{ product.prdName }}</a>
-          </td>
-          <td>
-            <v-row justify="center">
-              <v-dialog v-model="product.dialog" max-width="290">
-                <template v-slot:activator="{ on }">
-                  <v-btn
-                    icon
-                    color="red"
-                    href="#"
-                    data-toggle="modal"
-                    data-target="#modalDelete"
-                    v-on="on"
-                  >
-                    <v-icon>mdi-delete</v-icon>
-                  </v-btn>
-                </template>
-                <v-card>
-                  <v-card-title class="headline">삭제하시겠습니까?</v-card-title>
-                  <v-card-text>{{ product.prdName }}</v-card-text>
-                  <v-card-actions>
-                    <v-spacer></v-spacer>
-                    <v-btn color="blue-grey lighten-2" text @click="product.dialog = false">No</v-btn>
-                    <v-btn color="light-blue" text @click="removeEvent(product)">Yes</v-btn>
-                  </v-card-actions>
-                </v-card>
-              </v-dialog>
-            </v-row>
-          </td>
-        </tr>
-      </tbody>
-    </template>
-  </v-simple-table>
+  <v-container>
+    <v-row justify="center">
+      <v-col cols="12">
+        <v-simple-table id="product-table">
+          <template v-slot:default>
+            <thead>
+              <tr>
+                <th class="text-left">#</th>
+                <th class="text-left">상품 이미지</th>
+                <th class="text-left">상품명</th>
+                <th class="text-left">삭제</th>
+              </tr>
+            </thead>
+            <tbody id="product-rows">
+              <tr v-for="(product, index) in products" :key="index">
+                <th scope="row">{{ product.prdId }}</th>
+                <td>
+                  <img :src="product.prdImg" width="100%" />
+                </td>
+                <td>
+                  <a :href="product.prdUrl" target="_blank">{{ product.prdName }}</a>
+                </td>
+                <td>
+                  <v-row justify="center">
+                    <v-dialog v-model="product.dialog" max-width="290">
+                      <template v-slot:activator="{ on }">
+                        <v-btn
+                          icon
+                          color="red"
+                          href="#"
+                          data-toggle="modal"
+                          data-target="#modalDelete"
+                          v-on="on"
+                        >
+                          <v-icon>mdi-delete</v-icon>
+                        </v-btn>
+                      </template>
+                      <v-card>
+                        <v-card-title class="headline">삭제하시겠습니까?</v-card-title>
+                        <v-card-text>{{ product.prdName }}</v-card-text>
+                        <v-card-actions>
+                          <v-spacer></v-spacer>
+                          <v-btn color="blue-grey lighten-2" text @click="product.dialog = false">No</v-btn>
+                          <v-btn color="light-blue" text @click="removeEvent(product)">Yes</v-btn>
+                        </v-card-actions>
+                      </v-card>
+                    </v-dialog>
+                  </v-row>
+                </td>
+              </tr>
+            </tbody>
+          </template>
+        </v-simple-table>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
@@ -83,5 +89,71 @@ export default {
 a {
   text-decoration: none;
   color: black;
+}
+
+/*
+##Device = Desktops
+##Screen = 1281px to higher resolution desktops
+##Device = 데스크탑
+##Screen = 1281px 이상 해상도 데스크탑
+*/
+@media (min-width: 1281px) {
+  td:nth-child(2) img {
+    width: 100%;
+    max-width: 100px;
+  }
+}
+
+/*
+##Device = Laptops, Desktops
+##Screen = B/w 1025px to 1280px
+##Device = 랩탑, 데스크탑
+##Screen = 1025px에서 1280px 사이
+*/
+@media (min-width: 1025px) and (max-width: 1280px) {
+  td:nth-child(2) img {
+    width: 100%;
+    max-width: 100px;
+  }
+}
+
+/*
+##Device = Tablets, Ipads (portrait),
+##Screen = B/w 768px to 1024px
+##Device = 태블릿, 아이패드(세로),
+##Screen = 768px에서 1024px 사이
+*/
+@media (min-width: 768px) and (max-width: 1024px) {
+  td:nth-child(2) img {
+    width: 100%;
+    max-width: 100px;
+  }
+}
+
+/*
+##Device = Tablets, Ipads (landscape)
+##Screen = B/w 768px to 1024px
+##Device = 태블릿, 아이패드(가로)
+##Screen = 768px에서 1024px 사이
+*/
+@media (min-width: 768px) and (max-width: 1024px) and (orientation: landscape) {
+}
+
+/*
+##Device = Low Resolution Tablets, Mobiles (Landscape)
+##Screen = B/w 481px to 767px
+##Device = 저해상도 태블릿, 모바일(가로)
+##Screen = 481px에서 767px 사이
+*/
+@media (min-width: 481px) and (max-width: 767px) {
+}
+
+/*
+##Device = Most of the Smartphones Mobiles (Portrait)
+##Screen = B/w 320px to 479px
+##Device = 대부분의 스마트폰 모바일 기기(세로)
+##Screen = 320px에서 479px 사이
+*/
+@media (min-width: 320px) and (max-width: 480px) {
 }
 </style>
