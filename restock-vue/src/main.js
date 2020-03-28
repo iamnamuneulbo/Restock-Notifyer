d262511bd536357b5d19b35ea6620c78d1056703
leesk9719 <leesk9719@gmail.com>
2020-03-29 01:28:31 +0900

    firebase 추가

--- restock-vue/src/main.js
+++ restock-vue/src/main.js
@@ -3,6 +3,18 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
+import firebase from 'firebase';
+
+var firebaseConfig = {
+  apiKey: "AIzaSyBkp9H7OCIXeofXhfToQxNesu9GGjSvV7U",
+  authDomain: "restocknotifyer.firebaseapp.com",
+  databaseURL: "https://restocknotifyer.firebaseio.com",
+  projectId: "restocknotifyer",
+  storageBucket: "restocknotifyer.appspot.com",
+  messagingSenderId: "462307051096",
+  appId: "1:462307051096:web:b5d4dd7592820103a2ac2d"
+};
+firebase.initializeApp(firebaseConfig);
 
 Vue.config.productionTip = false
 
